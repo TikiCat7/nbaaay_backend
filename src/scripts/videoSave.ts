f955f13c3da6c71e61e58934bde94999ceca9efa
watarukay <watarukay@gmail.com>
2018-02-02 13:11:29 +0900

    Fix video search logic

--- src/scripts/videoSave.ts
+++ src/scripts/videoSave.ts
@@ -58,8 +58,16 @@ async function findAndSaveYoutubeVideos(matchRepository, youtubeVideoRepository,
       await forEachSeries(todaysMatches, async(match) => {
         if(match.statusNum === 3 && matchNotFresh(match.endTimeUTC)) {
           console.log('match is finished and 10+ hours since ended, dont search for videos');
-        }
-        else if (match.statusNum === 2) {
+        } else  if(match.statusNum === 3 && !matchNotFresh(match.endTimeUTC)) {
+          console.log('ready to look for videos, match is over but < 10 hours since it ended');
+          console.log(TRI_CODE_TO_TEAM_NAME[match.hTeamTriCode], TRI_CODE_TO_TEAM_NAME[match.vTeamTriCode]);
+          const videos = await videoFromChannel(channelId, `${TRI_CODE_TO_TEAM_NAME[match.hTeamTriCode]} | ${TRI_CODE_TO_TEAM_NAME[match.vTeamTriCode]}`, moment(match.startTimeUTCString).toISOString());
+
+          if (videos.items.length > 0) {
+            await saveVideosToDB(matchRepository, youtubeVideoRepository, playerRepository, videos.items, match.id);
+            console.log(`finished saving videos for match: ${match.id}`)
+          }
+        } else if (match.statusNum === 2) {
           console.log('ready to look for videos, match is active');
           console.log(TRI_CODE_TO_TEAM_NAME[match.hTeamTriCode], TRI_CODE_TO_TEAM_NAME[match.vTeamTriCode]);
           const videos = await videoFromChannel(channelId, `${TRI_CODE_TO_TEAM_NAME[match.hTeamTriCode]} | ${TRI_CODE_TO_TEAM_NAME[match.vTeamTriCode]}`, moment(match.startTimeUTCString).toISOString());
