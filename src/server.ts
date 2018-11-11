769b0bc1e7d32faf7a8e5427409e8496e94b49da
Wataru Kay <watarukay@gmail.com>
2018-11-12 01:31:26 +0900

    Change port number back to 3000

--- src/server.ts
+++ src/server.ts
@@ -355,7 +355,7 @@ createConnection({
   });
   server.applyMiddleware({ app }); // app is from an existing express app
 
-  app.listen({ port: 4000 }, () =>
+  app.listen({ port: 3000 }, () =>
     console.log(
       `🚀 Server ready at http://localhost:4000${server.graphqlPath}`,
     ),
